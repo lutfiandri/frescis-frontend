0487ee15e779a4007d208d67e50b5f65211cfab7
Lutfi Andriyanto <lutfiandriyanto@synapsis.id>
2023-03-19 09:41:53 +0700

    feat(default-layout): prettify app bar

--- src/components/templates/AppBar.js
+++ src/components/templates/AppBar.js
@@ -1,5 +1,24 @@
-function AppBar() {
-  return <div>app bar</div>;
+import { useRouter } from 'next/router';
+import { TbArrowLeft } from 'react-icons/tb';
+import styles from './AppBar.module.css';
+
+function AppBar({ title, showBackButton }) {
+  const router = useRouter();
+
+  return (
+    <header className={styles.container}>
+      {showBackButton ? (
+        <div
+          role="button"
+          className={styles.back_button}
+          onClick={() => router.back()}
+        >
+          <TbArrowLeft size={24} fontWeight={600} />
+        </div>
+      ) : null}
+      <div className={styles.title}>{title}</div>
+    </header>
+  );
 }
 
 export default AppBar;
--- src/layouts/DefaultLayout.js
+++ src/layouts/DefaultLayout.js
@@ -1,6 +1,5 @@
-import { Button, Space, Typography } from 'antd';
+import AppBar from '@/components/templates/AppBar';
 import { useRouter } from 'next/router';
-import { TbArrowLeft } from 'react-icons/tb';
 import BaseLayout from './BaseLayout';
 
 function DefaultLayout({
@@ -22,34 +21,7 @@ function DefaultLayout({
         }}
       >
         {!hideAppBar ? (
-          <header style={{ padding: '16px' }}>
-            <Space
-              direction="horizontal"
-              // align="center"
-              style={{
-                justifyContent: showBackButton ? 'left' : 'center',
-                alignItems: 'center',
-                width: '100%',
-                height: '32px',
-              }}
-            >
-              {showBackButton ? (
-                <Button
-                  size="small"
-                  type="link"
-                  icon={<TbArrowLeft size={24} fontWeight={600} />}
-                  style={{
-                    color: 'black',
-                  }}
-                  onClick={() => router.back()}
-                ></Button>
-              ) : // <TbArrowLeft size={20} fontWeight={600} />
-              null}
-              <Typography.Title level={5} style={{ margin: '0' }}>
-                {title}
-              </Typography.Title>
-            </Space>
-          </header>
+          <AppBar title={title} showBackButton={showBackButton}></AppBar>
         ) : null}
 
         <div
